8dd6aa4d790dc981b62f7e7e72f2ffaa95e579a4
Dean Johnson <deanjohnson222@gmail.com>
2015-01-13 16:23:47 -0800

    Added Algorithms 1 and 2.
    There is some commented out code for alg3 until
    we have it working.

--- assign1/assignment1.py
+++ assign1/assignment1.py
@@ -5,10 +5,23 @@ import sys
 
 
 def alg1(arr):
-    pass
+    _max = 0
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            _sum = sum(arr[i:j+1])
+            if _sum > _max:
+                _max = _sum
+    return _max
 
 def alg2(arr):
-    pass
+    _max = 0
+    for i in range(len(arr)):
+        _sum = 0
+        for j in range(i, len(arr)):
+            _sum += arr[j]
+            if _sum > _max:
+                _max = _sum
+    return _max
 
 def alg3(arr):
     pass
@@ -17,14 +30,22 @@ if __name__ == '__main__':
     if not len(sys.argv) > 1:
         print 'Usage: python assignment1.py [option]'
         print 'Options:'
-        options = ['--test', '--time', '--solve']
+        options = ['--test', '--time']
         print '\n'.join(map(lambda x:'\t'+ x, options))
         sys.exit()
+
     elif sys.argv[1] == '--test':
-        test_set_url = 'http://web.engr.oregonstate.edu/~glencora/cs325/mstest.txt'
-        test_sets = urllib2.urlopen(test_set_url).read().strip()
-        for test_set in test_sets.split('\n'):
-            arr, ans = eval(test_set)
+        test_sets = open('test_data.txt', 'r').read().strip()
+        for test_set in test_sets.split('\n\n'):
+            inp = map(int, test_set.replace(',', '').replace('[', '').replace(']', '').strip().split(' '))
+            arr, ans = inp[:-1], inp[-1]
+            print 'Input: {0}'.format(arr)
+            alg1_ans = alg1(arr)
+            print 'Alg1: Got %d expected %d' % (alg1_ans, ans)
+            # assert(alg1(arr) == ans)
+            alg2_ans = alg2(arr)
+            print 'Alg2: Got %d expected %d' % (alg2_ans, ans)
+            assert(alg2(arr) == ans)
             # Test here
 
     elif sys.argv[1] == '--time':
@@ -34,13 +55,15 @@ if __name__ == '__main__':
         for size in xrange(100, 1001, 100):
             arr = [random.randint(0, 100) for x in xrange(size)]
             timing = []
-            for alg in [alg1, alg2, alg3]:
+            for alg in [alg1, alg2]:
                 start = time.clock()
                 alg(arr)
                 timing.append(time.clock() - start)
-            timing = [size] + timing
+            timing = [size] + timing + ['']
             print timing_format.format(*timing)
 
+        '''
+        #  Alg3 Only.
         for size in xrange(2000, 9001, 1000):
             arr = [random.randint(0, 100) for x in xrange(size)]
             lines = sorted(map(lambda x: Line(*x), zip(ms, bs)), key=lambda x: x.m)
@@ -50,3 +73,4 @@ if __name__ == '__main__':
             timing.append(time.clock() - start)
             timing = [size, '', ''] + timing + [0]
             print timing_format.format(*timing)
+        '''
