48db42cb417688c8e2c2bf42ddd0c5dcb8ee6725
Dean Johnson <deanjohnson222@gmail.com>
2015-01-14 20:15:30 -0800

    Added graph pdf.

--- assign1/graph.py
+++ assign1/graph.py
@@ -1,32 +1,29 @@
+import matplotlib
+matplotlib.use('pdf')
+
 import matplotlib.pyplot as plt
-from pylab import *
 
 
 def make_graph(vals):
     xs = vals[0]
     alg1, alg2, alg3 = vals[1:]
     f, subplots = plt.subplots(2, 1, sharex='row')
+    f.set_tight_layout(True)
+    f.subplots_adjust(hspace=2.25)
+    plt.suptitle('Max Subarray Experimental Runtime Analysis')
+    subplots[0].set_title('Linear Axes')
     subplots[0].plot(xs[:9], alg1[:9], color='blue')
     subplots[0].plot(xs, alg2, color='green')
     subplots[0].plot(xs, alg3, color='red')
+    subplots[0].set_xlabel('Input Size')
+    subplots[0].set_ylabel('Average Runtime (10 Runs)')
+    subplots[1].set_title('Logarithmic Axes')
     subplots[1].loglog(xs[:10], alg1[:10], color='blue')
     subplots[1].plot(xs, alg2, color='green')
     subplots[1].plot(xs, alg3, color='red')
+    subplots[1].set_ylabel('Average Runtime (10 Runs)')
+    subplots[1].set_xlabel('Input Size')
 
-    # ax = fig.add_subplot(1,1,1)
-    # bx = fig.add_subplot(2,1,1)
-
-    # for val in vals:
-    #   line = ax.plot(*val, color='blue', lw=2)
-        # line = bx.plot(*val, color='red', lw=2)
-    # line2 = bx.plot(
-
-    # ax.set_yscale('log')
-    # ax.set_xscale('log')
-
-    # for val in vals:
-    #     ax.plot(*val)
-    #     print val
     plt.savefig('timings.pdf')
 
 if __name__ == '__main__':
