aed97a2127541fbcf3e0e2e4674ac5662e483163
Chance Zibolski <chance.zibolski@gmail.com>
2015-03-09 18:02:31 -0700

    Refactor

--- assign4/assignment4.py
+++ assign4/assignment4.py
@@ -4,59 +4,67 @@ import math
 from pulp import *
 
 
-prob = LpProblem("Local Temperature Change", LpMinimize)
-x0 = LpVariable("x0", 0)
-x1 = LpVariable("x1", 0)
-x2 = LpVariable("x2", 0)
-x3 = LpVariable("x3", 0)
-x4 = LpVariable("x4", 0)
-x5 = LpVariable("x5", 0)
-z = LpVariable("z")
-prob += z
-prob += z >= 0
-
-def T(day):
-    global prob, x0, x1, x2, x3, x4, x5
+def t(day, x0, x1, x2, x3, x4, x5):
     return (
-        (x5*math.sin((2*math.pi*day)/(365.25 * 10.7))) +
-        (x4*math.cos((2*math.pi*day)/(365.25 * 10.7))) +
-        (x3*math.sin((2*math.pi*day)/365.25)) +
-        (x2*math.cos((2*math.pi*day)/365.25)) +
-        (x1*day) +
+        (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7))) +
+        (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) +
+        (x3 * math.sin((2 * math.pi * day) / 365.25)) +
+        (x2 * math.cos((2 * math.pi * day) / 365.25)) +
+        (x1 * day) +
         x0
     )
 
 
-def main():
-    global prob, x0, x1, x2, x3, x4, x5
-    with open(sys.argv[1], 'rb') as csvfile:
+def get_differences(rows, xs):
+    diffs = []
+    for row in rows:
+        avg, day = row
+        diffs.append(avg - t(day, *xs))
+    return diffs
+
+
+def solve(rows):
+    prob = LpProblem("Local Temperature Change", LpMinimize)
+    z = LpVariable("z")
+    xs = [LpVariable("x%d" % i, 0) for i in xrange(6)]
+
+    prob += z
+    prob += z >= 0
+
+    diffs = get_differences(rows, xs)
+    for diff in diffs:
+        # print diff
+        # print -diff
+        # return
+        prob += (z >= diff)
+        prob += (z >= -diff)
+
+    status = prob.solve()
+    return LpStatus[status], map(value, xs)
+
+
+def process_file(f):
+    rows = []
+    with open(f, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
-        headers = reader.next()
-        rows = list(reader)
-
-        for row in rows:
-            avg, day = float(row[-2]), float(row[-1])
-            prob += z >= (avg - T(day))
-            prob += z >= -(avg - T(day))
-
-        status = prob.solve()
-        print "Status %s" % LpStatus[status]
-        print "Objective %s" % prob.objective
-        print "Objective (value) %s" % value(prob.objective)
-        print "Values:"
-        print value(x0)
-        print value(x1)
-        print value(x2)
-        print value(x3)
-        print value(x4)
-        print value(x5)
-        print value(z)
+        # Skip the header
+        reader.next()
+        for row in reader:
+            rows.append(map(float, row[-2:]))
+    return rows
 
 
-if len(sys.argv) < 2:
-    exit('Please provide an input file.')
-main()
+def main(args):
+    rows = process_file(args[0])
+    status, lp_x_values = solve(rows)
+
+    print "Status %s" % status
+    print "Values:"
+    for i in xrange(6):
+        print "x%d value: %s" % (i, lp_x_values[i])
 
 
-
-
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit('Please provide an input file.')
+    main(sys.argv[1:])
--- assign4/graph.py
+++ assign4/graph.py
@@ -1,20 +1,11 @@
-import csv
-import math
 import sys
 
 import matplotlib
-from scipy import stats
 matplotlib.use('ps')
 
-import numpy as np
 import matplotlib.pyplot as plt
+import assignment4
 
-x0 = 7.5576517
-x1 = 0.00023193911
-x2 = 4.9302622
-x3 = 8.4965903
-x4 = 0.0
-x5 = 0.0
 
 def make_graph(x1s, y1s, x2s, y2s):
     plt.plot(x1s, y1s, color='blue')
@@ -26,48 +17,24 @@ def make_graph(x1s, y1s, x2s, y2s):
     plt.savefig('timings.ps')
 
 
-def get_original_data_points():
-    with open(sys.argv[1], 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        headers = reader.next()
-        rows = list(reader)
-
-        avgs, days = [], []
-        for row in rows:
-            avgs.append(float(row[-2]))
-            days.append(float(row[-1]))
-        return days, avgs
-
-
-def T(day):
-    global x0, x1, x2, x3, x4, x5
-    return (
-        (x5*math.sin((2*math.pi*day)/(365.25 * 10.7))) +
-        (x4*math.cos((2*math.pi*day)/(365.25 * 10.7))) +
-        (x3*math.sin((2*math.pi*day)/365.25)) +
-        (x2*math.cos((2*math.pi*day)/365.25)) +
-        (x1*day) +
-        x0
-    )
-
-def get_calculated_data_points():
-    with open(sys.argv[1], 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        headers = reader.next()
-        rows = list(reader)
-
-        avgs, days = [], []
-        for row in rows:
-            day = float(row[-1])
-            avgs.append(T(day))
-            days.append(day)
-        return days, avgs
+def get_calculated_data_points(days, lp_x_values):
+    return [assignment4.t(day, *lp_x_values) for day in days]
 
 
 if __name__ == '__main__':
-    if not len(sys.argv) > 1:
+    if len(sys.argv) < 1:
         print 'Provide input data.'
         exit(0)
-    x1s, y1s = get_original_data_points()
-    x2s, y2s = get_calculated_data_points()
-    make_graph(x1s, y1s, x2s, y2s)
+
+    rows = assignment4.process_file(sys.argv[1])
+    days, avgs = [], []
+    for row in rows:
+        avg, day = row
+        avgs.append(avg)
+        days.append(day)
+
+    _, lp_x_values = assignment4.solve(rows)
+
+    computed_avgs = list(get_calculated_data_points(days, lp_x_values))
+
+    make_graph(days, avgs, days, computed_avgs)
